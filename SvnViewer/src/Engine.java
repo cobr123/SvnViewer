cf59d165b4cd91bc54f6422b0a740448fe17d84c
cobr123 <safebuster2@gmail.com>
2012-06-12 18:41:15 +0400

    init

---
+++ SvnViewer/src/Engine.java
@@ -0,0 +1,97 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import javax.swing.JButton;
+import javax.swing.SwingWorker;
+import javax.swing.table.DefaultTableModel;
+
+import org.tmatesoft.svn.core.SVNDirEntry;
+import org.tmatesoft.svn.core.SVNException;
+
+public class Engine implements ActionListener {
+	final MainWindow parent;
+	final DisplayRepositoryTree svnRep = new DisplayRepositoryTree(
+			"http://svn.svnkit.com/repos/svnkit/trunk/", "anonymous",
+			"anonymous");
+
+	public Engine(final MainWindow window) {
+		this.parent = window;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Get the source of this action
+		if (e.getSource() instanceof JButton) {
+			JButton clickedButton = (JButton) e.getSource();
+			if (clickedButton == parent.btnRefresh) {
+				parent.btnRefresh.setEnabled(false);
+				(new FileListRefresher()).execute();
+			} else if (clickedButton == parent.btnInsert) {
+				int rows[] = parent.tblFiles.getSelectedRows();
+				for (int i = rows.length - 1; i >= 0; --i) {
+					if (rows[i] >= 0) {
+						DefaultTableModel model = (DefaultTableModel) parent.tblFiles
+								.getModel();
+						String rowVal = (String) model.getValueAt(rows[i], 0);
+						model.removeRow(rows[i]);
+
+						DefaultTableModel modelForSave = (DefaultTableModel) parent.tblSaveFiles
+								.getModel();
+						modelForSave.insertRow(0, new Object[] { rowVal });
+					}
+				}
+			}
+		}
+	}
+
+	class FileListRefresher extends SwingWorker<Object, Object> {
+		private Collection entries;
+
+		@Override
+		public String doInBackground() {
+			System.out.println("doInBackground");
+			try {
+				entries = svnRep.GetEntries();
+			} catch (SVNException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void done() {
+			try {
+				parent.btnRefresh.setEnabled(true);
+				DefaultTableModel model = new DefaultTableModel();
+				Iterator iterator = entries.iterator();
+				model.addColumn("File name");
+
+				DefaultTableModel modelForSave = (DefaultTableModel) parent.tblSaveFiles
+						.getModel();
+				int cnt = modelForSave.getRowCount();
+				HashSet<String> set = new HashSet<String>();
+				for (int i = 0; i < cnt; ++i) {
+					if (!set.contains((String) modelForSave.getValueAt(i, 0))) {
+						set.add((String) modelForSave.getValueAt(i, 0));
+					}
+				}
+
+				while (iterator.hasNext()) {
+					SVNDirEntry entry = (SVNDirEntry) iterator.next();
+					// System.out.println(entry.getName());
+					if (!set.contains(entry.getName())) {
+						model.insertRow(0, new Object[] { entry.getName() });
+					}
+				}
+				parent.tblFiles.setModel(model);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println("done");
+		}
+	}
+}
---
+++ SvnViewer/src/MainWindow.java
@@ -0,0 +1,111 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+
+import javax.swing.BoxLayout;
+import javax.swing.DropMode;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
+
+public class MainWindow {
+	private JTextField fldSvnUrl;
+	final JButton btnRefresh;
+	final JButton btnInsert;
+	private JPanel p1, p3;
+	JTable tblFiles;
+	JTable tblSaveFiles;
+
+	public MainWindow() {
+		String[] petStrings = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
+		Engine engine = new Engine(this);
+
+		JPanel pnlChooseSvnFiles = new JPanel();
+
+		// Set the layout manager for this panel
+		BorderLayout bl = new BorderLayout();
+		pnlChooseSvnFiles.setLayout(bl);
+
+		fldSvnUrl = new JTextField("http://svn.svnkit.com/repos/svnkit/trunk/");
+		fldSvnUrl.setHorizontalAlignment(JTextField.LEFT);
+
+		btnRefresh = new JButton("Обновить");
+		btnRefresh.addActionListener(engine);
+
+		p3 = new JPanel();
+		BoxLayout gl = new BoxLayout(p3, BoxLayout.X_AXIS);
+		p3.setLayout(gl);
+
+		p3.add(fldSvnUrl);
+		p3.add(btnRefresh);
+
+		p1 = new JPanel();
+		BoxLayout gl1 = new BoxLayout(p1, BoxLayout.Y_AXIS);
+		p1.setLayout(gl1);
+
+		p1.add(p3);
+
+		JComboBox petList = new JComboBox(petStrings);
+		p1.add(petList);
+
+		pnlChooseSvnFiles.add(p1, BorderLayout.NORTH);
+
+		// tblFiles = new JTable(data, columnNames);
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("File name");
+		tblFiles = new JTable(model);
+		tblFiles.setAutoCreateRowSorter(true);
+		// Create the scroll pane and add the table to it.
+		JScrollPane scrollPane = new JScrollPane(tblFiles);
+		// Add the scroll pane to this panel.
+		pnlChooseSvnFiles.add(scrollPane, BorderLayout.CENTER);
+
+		JPanel p3 = new JPanel();
+		BoxLayout gl3 = new BoxLayout(p3, BoxLayout.X_AXIS);
+		p3.setLayout(gl3);
+		pnlChooseSvnFiles.add(p3, BorderLayout.SOUTH);
+
+		btnInsert = new JButton("Выбрать");
+		btnInsert.addActionListener(engine);
+		p3.add(btnInsert);
+		
+		JPanel pnlSaveFiles = new JPanel();
+		BoxLayout gl2 = new BoxLayout(pnlSaveFiles, BoxLayout.Y_AXIS);
+		pnlSaveFiles.setLayout(gl2);
+		DefaultTableModel model2 = new DefaultTableModel();
+		model2.addColumn("File name");
+		tblSaveFiles = new JTable(model2);
+		tblSaveFiles.setAutoCreateRowSorter(true);
+		JScrollPane scrollPane2 = new JScrollPane(tblSaveFiles);
+		pnlSaveFiles.add(scrollPane2);
+		
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Tab 1", pnlChooseSvnFiles);
+        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+        tabbedPane.addTab("Tab 2", pnlSaveFiles);
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+        tabbedPane.setTabPlacement(JTabbedPane.BOTTOM);
+        
+		// Create the frame and set its content pane
+		JFrame frame = new JFrame("Svn Viewer");
+		frame.setContentPane(tabbedPane);
+		// Set the size of the window to be big enough to accommodate all
+		// controls
+		frame.pack();
+
+		// Display the window
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		MainWindow window = new MainWindow();
+	}
+}
